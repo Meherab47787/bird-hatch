89bf42121dade6fdda028a124c7c8c542c991dbc
Meherab Hassan <hassan47787@gmail.com>
2024-02-27 23:23:30 +0600

    protected CORS

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 dotenv.config({ path: './.env' });
 
 const corsOptions = {
-    origin: 'https://bird-hatch-tv4i.vercel.app',
+    origin: process.env.CORS_ORIGIN,
     methods: 'GET,DELETE,PATCH,POST,PUT',
     credentials: true,
     allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
